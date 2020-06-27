c01b596b8b771bf504af1d9416a37cb56a88301a
Brandon Wong <bwong7@gmu.edu>
2020-06-26 23:19:45 -0400

    SongPlayer plays songs

--- SongPlayer.py
+++ SongPlayer.py
@@ -4,14 +4,15 @@ import pygame as pg
 
 from Component import Component
 from Actions import Actions
+import ResourceManager
 
 class SongPlayer(Component):
-    def __init__(self, gctxt):
+    def __init__(self, gctxt, current_song=None):
         Component.__init__(self, gctxt.screen_rect.x, gctxt.screen_rect.y, gctxt.screen_rect.w, gctxt.screen_rect.h)
         
         self.song_queue = queue.Queue() # Thread Safe Queue
         # Cant peek a queue.Queue(), so just store the current and next here
-        self.current_song = None
+        self.current_song = current_song
         self.next_song = None
 
         # Default Black Background
@@ -56,17 +57,18 @@ class SongPlayer(Component):
         self.current_lbl = Component(0,0,0,0, parent=self)
         self.current_lbl.set_font("calibri", size=self.font_size/2)
         self.current_lbl.text_color = (255,255,255)
-        self.current_lbl.set_text("Current Song", allow_resize=True)
+        self.current_lbl.set_text(self.current_song, allow_resize=True)
         self.current_lbl.flush_top()
         self.current_lbl.rect.y += 16
         self.current_lbl.flush_left()
         self.current_lbl.rect.x += 16
+        self.current_lbl.register_event(Actions.on_left_click, self.play, member_func=True)
 
         # Next Song
         self.next_lbl = Component(0,0,0,0, parent=self)
         self.next_lbl.set_font("calibri", size=self.font_size/2)
         self.next_lbl.text_color = (255,255,255)
-        self.next_lbl.set_text("Next Song", allow_resize=True)
+        self.next_lbl.set_text(self.next_song, allow_resize=True)
         self.next_lbl.flush_top()
         self.next_lbl.rect.y += 16
         self.next_lbl.flush_right()
@@ -79,3 +81,9 @@ class SongPlayer(Component):
     def open_settings(self, gctxt):
         # TODO Implement
         print("Open settings...")
+
+    def play(self, gctxt):
+        #sound = ResourceManager.get_audio(self.current_song)
+        print('playing {}'.format(self.current_song))
+        pg.mixer.music.load(ResourceManager.get_music_path(self.current_song))
+        pg.mixer.music.play(0)
