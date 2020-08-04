8a7f1af6cbf541e967205088b3a2aca42b0712bc
Brandon Wong <bwong7@gmu.edu>
2020-08-03 23:33:05 -0400

    Somehow forgot to add some of the files in the previous commit

--- Song.py
+++ Song.py
@@ -17,6 +17,30 @@ class Song:
 
         self.sound_path = ""
 
+    def load(filename):
+        self.filename = filename
+        if os.path.exists(filename) and os.path.isfile(filename):
+            with open(filename, 'rb') as f:
+
+            magic = f.read(3)
+            if magic != b"DPT":
+                return False, "Magic is Wrong"
+            
+            self.song_title = str(f.read(256), encoding='utf-8')
+            self.artist = str(f.read(128), encoding='utf-8')
+            # unsigned short - uint16
+            self.genre = Song.parseGenreID(struct.unpack("<H",f.read(2)))
+            # unsigned int - uint32
+            self.uuid = struct.unpack("<I", f.read(4))
+
+            # TODO
+            # CRC??
+
+            # unsigned short - uint16
+            self.num_lines = struct.unpack("<H", f.read(2))
+
+            # TODO the rest of the file. What does it look like fuck
+
     def get_lyrics():
         pass
 
