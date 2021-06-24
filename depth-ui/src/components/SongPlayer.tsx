12cbe148059e8559f09bc10bcbac681b5c5144db
yevhenp <yevhenp@vt.edu>
2021-06-24 01:25:27 -0400

    Set up React UI with ALL STAR

---
+++ depth-ui/src/components/SongPlayer.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import lyricsJson from '../songs/all-star.json';
+
+export default function SongPlayer() {
+	const audioRef = React.useRef<HTMLAudioElement>(null);
+	const [timeElapsed, setTimeElapsed] = React.useState(0);
+	const [showLyrics, setShowLyrics] = React.useState("...");
+	const [showNextLine, setShowNextLine] = React.useState("...");
+
+	const lyricsMap = JSON.parse(JSON.stringify(lyricsJson))['mapping'];
+
+	function updateTime() {
+		if(audioRef && audioRef.current) {
+			setTimeElapsed(Math.round(audioRef.current.currentTime * 1000));
+		}
+	}
+
+	React.useEffect(() => {
+		for (const [i, lyricObj] of lyricsMap.entries()) {
+			if (timeElapsed >= lyricObj.start && timeElapsed < lyricObj.end) {
+				setShowLyrics(lyricObj.lyrics);
+				setShowNextLine(lyricsMap[i + 1]?.lyrics ?? '...');
+				break;
+			}
+			else {
+				setShowLyrics('...');
+			}
+		}
+	}, [lyricsMap, timeElapsed]);
+
+	return (
+		<div>
+			<h3>{timeElapsed}</h3>
+			<audio ref={audioRef} controls onTimeUpdate={updateTime}>
+				<source src={`${process.env.PUBLIC_URL}/all-star.mp3`} type="audio/mpeg"/>
+				Your browser does not support the audio element.
+			</audio>
+			<h2>{showLyrics}</h2>
+			<h4>{showNextLine}</h4>
+		</div>
+	);
+}
