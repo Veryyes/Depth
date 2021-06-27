e0d74fe1649c3dc22ecdac474be46e696ff8a71e
yevhenp <yevhenp@vt.edu>
2021-06-27 16:36:52 -0400

    make it look pretty

--- depth-ui/src/App.js
+++ depth-ui/src/App.js
@@ -3,15 +3,14 @@ import './App.css';
 import SongPlayer from './components/SongPlayer';
 
 function App() {
-
-    return (
-		<div className = "App" >
-        <header className = "App-header" >
-        	<img src = { logo } className = "App-logo" alt = "logo" / >
-        	<SongPlayer / >
-        </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <SongPlayer />
+      </header>
+    </div>
+  );
 }
 
 export default App;
